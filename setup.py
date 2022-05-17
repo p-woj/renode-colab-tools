5a58f06a186cad3231075328dcc9fa94fe47daab
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2022-05-17 02:50:05 +0200

    Update contact email

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='renode_colab_tools',
       version='0.1',
       description='Python helper lib for Renode in Colab',
       author='Antmicro',
-      author_email='dwojciechowski@antmicro.com',
+      author_email='contact@antmicro.com',
       license='MIT',
       packages=['renode_colab_tools'])
